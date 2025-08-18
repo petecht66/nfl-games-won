6f7f70e432768fbb7e4fe2c05248fe7dec4c6baf
Peter <petecht66@gmail.com>
2025-08-18 02:54:59 -0400

    reading in 2024 data set from main branch on GitHub

---
+++ R-scripts/2024-stepwise-selection.r
@@ -0,0 +1,8 @@
+
+# R packages needed for this script
+library(ggplot2)
+
+# importing 2024 NFL regular season data set from main branch on GitHub
+regular_season_2024 <- read.csv("https://raw.githubusercontent.com/petecht66/nfl-games-won/refs/heads/main/data/2024%20NFL%20Games%20Won.csv")
+regular_season_2024
+
